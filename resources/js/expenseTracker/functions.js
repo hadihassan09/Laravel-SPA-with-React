c3c7f82be44014d62176c98e028f3b1106618e59
Hadi Hassan <hadihassan09@gmail.com>
2020-11-19 00:39:28 +0200

    added function to change date to readable format

--- resources/js/expenseTracker/functions.js
+++ resources/js/expenseTracker/functions.js
@@ -1,5 +1,11 @@
-function capitalizeFLetter(word){
+import moment from 'moment';
+
+export const capitalizeFLetter = (word)=>{
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-export default capitalizeFLetter;
+
+export const formatDate = (date)=>{
+    return moment(date, 'YYYY-MM-DD HH:mm:ss').format('dddd,DD MMMM, h:mm:ss a');;
+}
+
