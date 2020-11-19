336b374b198559a77c1351448c5891e8aa256f1a
Hadi Hassan <hadihassan09@gmail.com>
2020-11-19 13:45:31 +0200

    added edit expense route

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/expenses', [ExpenseController::class, 'index']);
 Route::middleware('auth:sanctum')->post('/expenses/create', [ExpenseController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/expenses/delete/{expense}', [ExpenseController::class, 'destroy']);
+Route::middleware('auth:sanctum')->post('/expenses/update/{expense}', [ExpenseController::class, 'update']);
