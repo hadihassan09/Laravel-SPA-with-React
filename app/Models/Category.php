0e4c0129267c5570ac4f6631b58365eb3bff0061
Hadi Hassan <hadihassan09@gmail.com>
2020-11-19 20:59:08 +0200

    added categories relation with expenses

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function expenses()
+    {
+        return $this->hasMany('App\Models\Expense');
+    }
 }
