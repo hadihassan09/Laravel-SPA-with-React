01a57165deaced35067b72d713a529dda17107e1
Hadi Hassan <hadihassan09@gmail.com>
2020-11-19 23:10:12 +0200

    added category component with its creation modal

---
+++ resources/js/expenseTracker/componenets/Categories/Categories.js
@@ -0,0 +1,280 @@
+import React from 'react';
+import axios from "axios";
+import {formatDate, capitalizeFLetter} from '../../functions'
+import CreateCategoryModal from "./createCategoryModal";
+
+class Categories extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={
+            index: 0,
+            categories: [],
+            showCreateModel: false,
+            status: false,
+            danger: false,
+            statusMessage: '',
+            showEditModel: false,
+            modalData: null
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/api/categories').then(response=>{
+            this.setState({
+                categories: response.data.categories
+            });
+        }).catch(error=>{
+            console.log(error);
+        });
+    }
+
+    //Category Functions
+
+    deleteCategory = (category)=>{
+        axios.get('api/categories/delete/'.concat(category.id)).then(response=>{
+            console.log(response);
+            if(response.status===200 && response.data.success === true) {
+                let index = this.state.categories.indexOf(category);
+                let oldCategories = this.state.categories.slice();
+                oldCategories.splice(index, 1);
+                this.setState({
+                    categories: oldCategories,
+                    status: true,
+                    danger: false,
+                    statusMessage: 'Category Deleted Successfully'
+                }, ()=>{
+                    setTimeout(()=>{
+                        this.setState({
+                            status: false
+                        })
+                    }, 3000);
+                });
+            }else{
+                this.setState({
+                    status: true,
+                    danger: true,
+                    statusMessage: 'Category Could Not Be Deleted'
+                }, ()=>{
+                    setTimeout(()=>{
+                        this.setState({
+                            status: false
+                        })
+                    }, 3000);
+                });
+            }
+        }).catch(error=>{
+            this.setState({
+                status: true,
+                danger: true,
+                statusMessage: 'Category Could Not Be Deleted'
+            }, ()=>{
+                setTimeout(()=>{
+                    this.setState({
+                        status: false
+                    })
+                }, 3000);
+            });
+        })
+    };
+
+
+
+    //Create Modal Functions
+    handleSaveCreateModel = (data, success)=>{
+        if(success === true){
+            this.setState({
+            });
+            this.setState({
+                categories: this.state.categories.concat(data),
+                showCreateModel: false,
+                status: true,
+                danger: false,
+                statusMessage: 'Category Added Successfully'
+            }, ()=>{
+                setTimeout(()=>{
+                    this.setState({
+                        status: false
+                    })
+                }, 3000);
+            });
+        }else{
+            this.setState({
+                showCreateModel: false,
+                status: true,
+                danger: true,
+                statusMessage: 'Category Could Not Be Added'
+            }, ()=>{
+                setTimeout(()=>{
+                    this.setState({
+                        status: false
+                    })
+                }, 3000);
+            });
+        }
+
+    };
+
+    showCreateModel = ()=>{
+        this.setState({
+            showCreateModel: true
+        })
+    };
+
+    closeCreateModel = ()=>{
+        this.setState({
+            showCreateModel: false
+        })
+    }
+
+    // //Edit Modal Functions
+    // handleSaveEditModal = (data, success) => {
+    //     if(success === true){
+    //         let oldCategories = this.state.categories;
+    //         oldCategories.splice(this.state.index, 1, data);
+    //         this.setState({
+    //             expenses: oldExpenses,
+    //             status: true,
+    //             danger: false,
+    //             showEditModel: false,
+    //             modalData: null,
+    //             statusMessage: 'Category Updated Successfully'
+    //         }, ()=>{
+    //             setTimeout(()=>{
+    //                 this.setState({
+    //                     status: false
+    //                 })
+    //             }, 3000);
+    //         });
+    //     }else{
+    //         this.setState({
+    //             status: true,
+    //             showEditModel: false,
+    //             modalData: null,
+    //             danger: true,
+    //             statusMessage: 'Expense Could Not Be Updated'
+    //         }, ()=>{
+    //             setTimeout(()=>{
+    //                 this.setState({
+    //                     status: false
+    //                 })
+    //             }, 3000);
+    //         });
+    //     }
+    // };
+    //
+    //
+    // handleShowEditModal = (expense) => {
+    //     this.setState({
+    //         index: this.state.expenses.indexOf(expense),
+    //         showEditModel: true,
+    //         modalData: expense
+    //     });
+    // };
+    //
+    // handleCloseEditModal = () => {
+    //     this.setState({
+    //         showEditModel: false,
+    //         modalData: null
+    //     });
+    // };
+    //
+    // renderEditModal = ()=>{
+    //     if(this.state.showEditModel === true)
+    //         return (
+    //             <EditExpenseModal
+    //                 show={this.state.showEditModel}
+    //                 title={"Edit Expense"}
+    //                 data={this.state.modalData}
+    //                 onClick={this.handleCloseEditModal}
+    //                 onHide={this.handleCloseEditModal}
+    //                 onSave={this.handleSaveEditModal}/>
+    //         );
+    //     else
+    //         return(
+    //             <>
+    //             </>
+    //         );
+    // }
+
+
+    render() {
+        return(
+            <div>
+                <div align="center">
+                    <h1 style={{textAlign: "center",color: "gray"}}>Welcome To Expense Tracker</h1>
+                    <p style={{textAlign: "center",color:"black"}}>Here are the List of Expenses</p>
+                    <br/><br/>
+                </div>
+                {
+                    this.state.status ?
+                        this.state.danger ?
+                            <div className="alert alert-danger wrapper inMiddle" style={{marginTop: 10, textAlign: 'center'}}>
+                                {this.state.statusMessage}
+                            </div>
+                            :
+                            <div className="alert alert-success wrapper inMiddle" style={{marginTop: 10, textAlign: 'center'}}>
+                                {this.state.statusMessage}
+                            </div>
+                        : ''
+                }
+                <div id="mainBody">
+                    <div>
+                        <a className="addButton" style={{cursor: "pointer"}}
+                           onClick={()=>{
+                               this.showCreateModel()
+                           }}>
+                            Add Category
+                        </a>
+                        <div>
+                            <table id="t01">
+                                <thead>
+                                <tr>
+                                    <th>Category Number</th>
+                                    <th>Category Name</th>
+                                    <th>Date</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.state.categories.map((category,index)=>
+                                    <tr key={category.id}>
+                                        <td>{index}</td>
+                                        <td>{capitalizeFLetter(category.name)}</td>
+                                        <td>{formatDate(category.created_at)}</td>
+                                        <td>
+                                            <div className={"h3"}>
+                                                <i style={{marginRight: 15}}
+                                                   className="fas fa-edit text-blue-200 hover:text-blue-600 cursor-pointer"
+                                                   onClick={() => {
+                                                       this.handleShowEditModal(category);
+                                                   }}></i>
+                                                <i
+                                                    className="fas fa-times text-red-200 hover:text-red-600 cursor-pointer"
+                                                    onClick={() => {
+                                                        this.deleteCategory(category);
+                                                    }}></i>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <CreateCategoryModal
+                    show={this.state.showCreateModel}
+                    title={"Create Category"}
+                    onClick={this.closeCreateModel}
+                    onHide={this.closeCreateModel}
+                    onSave={this.handleSaveCreateModel}/>
+                {/*{ this.renderEditModal() }*/}
+            </div>
+        );
+    }
+
+}
+
+
+export default Categories;
---
+++ resources/js/expenseTracker/componenets/Categories/createCategoryModal.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import {Modal, Button, FormGroup, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import {capitalizeFLetter} from "../../functions";
+
+class CreateCategoryModal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            name: '',
+            nameError: false,
+        }
+    }
+
+    addCategory = ()=>{
+        axios.post('/api/categories/create',{
+            name: this.state.name,
+        }).then(response=>{
+            this.setState({
+                name: '',
+                nameError: false,
+            })
+            this.props.onSave(response.data.category, true);
+        }).catch(error=>{
+            try {
+                if (error.response && error.response.status === 422) {
+                    if (error.response.data.errors.name)
+                        this.setState({
+                            nameError: true
+                        });
+                }else{
+                    this.props.onSave(null, false);
+                }
+            }catch (e) {
+
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Modal show={this.props.show} onHide={() => this.props.onHide()}>
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            {this.props.title}
+                        </Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <FormGroup>
+                            <Form.Label>
+                                Category Name:
+                            </Form.Label>
+                            <Form.Control
+                                isInvalid={this.state.nameError}
+                                type="text"
+                                value={this.state.name}
+                                onChange={e=>this.setState({name: e.target.value})}
+                            />
+                            {
+                                this.state.nameError ?
+                                    <Form.Control.Feedback type="invalid">
+                                        Please enter a Category Name.
+                                    </Form.Control.Feedback>
+                                    :
+                                    ''
+                            }
+                        </FormGroup>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary"
+                                onClick={() => this.props.onClick()}>Close</Button>
+                        <Button variant="primary"
+                                onClick={this.addCategory}>Submit</Button>
+                    </Modal.Footer>
+
+                </Modal>
+            </div>
+        )
+    };
+}
+
+export default CreateCategoryModal;
