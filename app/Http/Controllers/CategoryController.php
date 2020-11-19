0facfc6aeda05fd744b38d989a5b88cba0882fbc
Hadi Hassan <hadihassan09@gmail.com>
2020-11-19 20:59:13 +0200

    added route for getting all categories added its equivelent function returning all records

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(
+            ['categories' => Category::all()]
+        );
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\ExpenseController;
+use \App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,7 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Expenses Routes
 Route::middleware('auth:sanctum')->get('/expenses', [ExpenseController::class, 'index']);
 Route::middleware('auth:sanctum')->post('/expenses/create', [ExpenseController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/expenses/delete/{expense}', [ExpenseController::class, 'destroy']);
 Route::middleware('auth:sanctum')->post('/expenses/update/{expense}', [ExpenseController::class, 'update']);
+
+
+//Categories Routes
+Route::middleware('auth:sanctum')->get('/categories', [CategoryController::class, 'index']);
